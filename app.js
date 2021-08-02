5430eec995143c7541eb430f7412f1fa33725b2c
reshmaparhar <parharra17.it@coep.ac.in>
2021-08-02 19:06:37 +0530

    added API Documentation

--- app.js
+++ app.js
@@ -20,14 +20,8 @@ const swaggerDefinition = {
           version: '1.0.0',
           description:
             'This is a REST API application made with Express. ',
-          license: {
-            name: 'Licensed Under MIT',
-            url: 'https://spdx.org/licenses/MIT.html',
-          },
-          contact: {
-            name: 'JSONPlaceholder',
-            url: 'https://jsonplaceholder.typicode.com',
-          },
+         
+          
         },
         servers: [
           {
